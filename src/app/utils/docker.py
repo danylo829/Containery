07f05cb9fabc8a6fa3950f742346db7f04c4836e
Danylo Havryliv <evg829@gmail.com>
2024-09-22 01:24:22 +0300

    Change default timeout

--- src/app/utils/docker.py
+++ src/app/utils/docker.py
@@ -31,7 +31,7 @@ class Docker:
             return exec_instance_json.get("Id")
         return None
 
-    def start_exec_session(self, exec_id, sid, socketio, docker_socket, timeout=5):
+    def start_exec_session(self, exec_id, sid, socketio, docker_socket, timeout=300):
         exec_start_endpoint = f"/exec/{exec_id}/start"
         start_payload = {"Detach": False, "Tty": True, "ConsoleSize": [44, 150]}
 
