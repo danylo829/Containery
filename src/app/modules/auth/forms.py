b4c561ab3bc704ab642ef443b18b12a2d693cc36
Danylo Havryliv <evg829@gmail.com>
2024-09-22 00:54:54 +0300

    Formatting

--- src/app/modules/auth/forms.py
+++ src/app/modules/auth/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()], )
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
