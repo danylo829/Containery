74eb8e1a056b73e97dae7f1c95a6b706311206ef
Danylo Havryliv <evg829@gmail.com>
2024-09-30 23:52:19 +0300

    [refactor] [config] Migrate to environment variables

--- src/app/config.py
+++ src/app/config.py
@@ -1,6 +1,8 @@
+import os
+
 class Config:
-    SECRET_KEY = '12345678'
-    CSRF_SECRET_KEY = '87654321'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////app_data/containery.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    CSRF_SECRET_KEY = os.getenv('CSRF_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:////app_data/containery.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', 'False') == 'True'
+    DEBUG = os.getenv('DEBUG', 'False') == 'True'
