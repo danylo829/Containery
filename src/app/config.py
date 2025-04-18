145eefa08e80ad39d8b05b60e70028b8c0ac13eb
Danylo Havryliv <evg829@gmail.com>
2025-04-18 15:50:14 +0300

    [fix] [config] Set default values for SECRET_KEY and CSRF_SECRET_KEY

--- src/app/config.py
+++ src/app/config.py
@@ -1,8 +1,9 @@
 from os import getenv
+import secrets
 
 class Config:
-    SECRET_KEY = getenv('SECRET_KEY')
-    CSRF_SECRET_KEY = getenv('CSRF_SECRET_KEY')
+    SECRET_KEY = getenv('SECRET_KEY', secrets.token_hex(32))
+    CSRF_SECRET_KEY = getenv('CSRF_SECRET_KEY', secrets.token_hex(32))
     SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:////app_data/containery.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = getenv('SQLALCHEMY_TRACK_MODIFICATIONS', 'False') == 'True'
     DEBUG = getenv('DEBUG', 'False') == 'True'
