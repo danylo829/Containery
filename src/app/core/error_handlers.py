7634f9bf73765630a4a0707403dcd3e33b18ba82
Danylo Havryliv <evg829@gmail.com>
2025-06-11 13:58:12 +0300

    [feat] [core]Enhance error handling for unauthenticated users

--- src/app/core/error_handlers.py
+++ src/app/core/error_handlers.py
@@ -1,9 +1,16 @@
 from flask import render_template
+from flask_login import current_user
 
 def page_not_found(e):
     code = 404
-    return render_template('error.html', code=code, message=e), code
+    if current_user.is_authenticated:
+        return render_template('error.html', code=code, message=e), code
+    else:
+        return render_template('error_unauthenticated.html', code=code, message=e), code
 
 def internal_server_error(e):
     code = 500
-    return render_template('error.html', code=code, message=e), code
+    if current_user.is_authenticated:
+        return render_template('error.html', code=code, message=e), code
+    else:
+        return render_template('error_unauthenticated.html', code=code, message=e), code
